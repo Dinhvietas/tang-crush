db108fb76b6461fa594c21d889f4a43979cc3fa3
Dinhvietas <92622959+Dinhvietas@users.noreply.github.com>
2021-12-21 20:28:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
 const CONFIG = {
-    introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Anh Nguyễn Đình Việt à!',
+    introDesc: `Lá cứ đến gần Đông là rụng
+                Em cứ đến gần anh là rung là động`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Có Con Khỉ Nhá',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
